c5db1fe6df31569c4b2d76f731a0d2ebe9693bca
fatencio <fatencio@ccxc.us>
2023-02-05 22:16:38 -0500

    Configuracion inicial

---
+++ src/test/java/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinition {
+    @Given("Sergey has an active account")
+    public void sergeyHasAnActiveAccount() {
+    }
+
+    @When("he sends their valid credentials")
+    public void heSendsTheirValidCredentials() {
+    }
+
+    @Then("he should have access to manage his account")
+    public void heShouldHaveAccessToManageHisAccount() {
+    }
+}
